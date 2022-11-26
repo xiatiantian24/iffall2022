f6977aca04170578b70b197910fc85ec7e070e8c
Ray Xia <77950209+xiatiantian24@users.noreply.github.com>
2022-11-25 21:53:57 -0600

    Add files via upload

--- microsite/fully-realized/script.js
+++ microsite/fully-realized/script.js
@@ -1,69 +1,75 @@
-// const scrollContainer = document.getElementById("js-scroll");
+const hamburger = document.querySelector('#hamburger')
+const navbar = document.querySelector('.navbar')
 
-// scrollContainer.addEventListener("wheel", (evt) => {
-//     evt.preventDefault();
-//     scrollContainer.scrollLeft += evt.deltaY;
-// });
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('show')
+})
+// // const scrollContainer = document.getElementById("js-scroll");
 
-(function () {
-    init();
+// // scrollContainer.addEventListener("wheel", (evt) => {
+// //     evt.preventDefault();
+// //     scrollContainer.scrollLeft += evt.deltaY;
+// // });
 
-    var g_containerInViewport;
-    function init() {
-        setStickyContainersSize();
-        bindEvents();
-    }
+// (function () {
+//     init();
 
-    function bindEvents() {
-        window.addEventListener("wheel", wheelHandler);
-    }
+//     var g_containerInViewport;
+//     function init() {
+//         setStickyContainersSize();
+//         bindEvents();
+//     }
 
-    function setStickyContainersSize() {
-        document.querySelectorAll('.js-sticky-container').forEach(function (container) {
-            const stikyContainerHeight = container.querySelector('.js-scroll').scrollWidth;
-            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
-        });
-    }
+//     function bindEvents() {
+//         window.addEventListener("wheel", wheelHandler);
+//     }
 
-    function isElementInViewport(el) {
-        const rect = el.getBoundingClientRect();
-        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-    }
+//     function setStickyContainersSize() {
+//         document.querySelectorAll('.js-sticky-container').forEach(function (container) {
+//             const stikyContainerHeight = container.querySelector('.js-scroll').scrollWidth;
+//             container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+//         });
+//     }
 
-    function wheelHandler(evt) {
+//     function isElementInViewport(el) {
+//         const rect = el.getBoundingClientRect();
+//         return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+//     }
 
-        const containerInViewPort = Array.from(document.querySelectorAll('.js-sticky-container')).filter(function (container) {
-            return isElementInViewport(container);
-        })[0];
+//     function wheelHandler(evt) {
 
-        if (!containerInViewPort) {
-            return;
-        }
+//         const containerInViewPort = Array.from(document.querySelectorAll('.js-sticky-container')).filter(function (container) {
+//             return isElementInViewport(container);
+//         })[0];
 
-        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
-        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
-        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+//         if (!containerInViewPort) {
+//             return;
+//         }
 
-        if (g_canScrollHorizontally) {
-            containerInViewPort.querySelector('.js-scroll').scrollLeft += evt.deltaY;
-        }
-    }
-})();
+//         var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+//         var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+//         let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
 
-var body = document.getElementsByClassName('js-scroll')[0];
-body.style.backgroundColor = '#F5834F';
+//         if (g_canScrollHorizontally) {
+//             containerInViewPort.querySelector('.js-scroll').scrollLeft += evt.deltaY;
+//         }
+//     }
+// })();
 
-// trigger this function every time the user scrolls
-window.onscroll = function (event) {
-    var scroll = window.pageYOffset;
-    if (scroll < 800) {
-        // green
-        body.style.backgroundColor = "#F5834F";
-    } else if (scroll >= 800 && scroll < 1500) {
-        // yellow
-        body.style.backgroundColor = '#39A5EC';
-    } else {
-        // blue
-        body.style.backgroundColor = '#A6D48C';
-    }
-}
+// var body = document.getElementsByClassName('js-scroll')[0];
+// body.style.backgroundColor = '#F5834F';
+
+// // trigger this function every time the user scrolls
+// window.onscroll = function (event) {
+//     var scroll = window.pageYOffset;
+//     if (scroll < 800) {
+//         // green
+//         body.style.backgroundColor = "#F5834F";
+//     } else if (scroll >= 800 && scroll < 1500) {
+//         // yellow
+//         body.style.backgroundColor = '#39A5EC';
+//     } else {
+//         // blue
+//         body.style.backgroundColor = '#A6D48C';
+//     }
+// }
